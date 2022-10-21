a64e1b9b02c89dcf7f47164654ab07af4c4e7ec5
Bruno <Brn.config@outlook.com>
2022-10-21 12:37:27 -0300

    JWT

--- CleanArch.API/Controllers/CategoriasController.cs
+++ CleanArch.API/Controllers/CategoriasController.cs
@@ -1,6 +1,5 @@
 ﻿using CleanArch.Application.DTOs;
 using CleanArch.Application.Interfaces;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -26,6 +25,55 @@ namespace CleanArch.API.Controllers
                 return NotFound("Categorias não existe");
 
             return Ok(categorias);
+        }
+
+        [HttpGet("{id:int}",Name ="GetCategoria")]
+        public async Task<ActionResult<CategoriaDTO>> Get(int id)
+        {
+            var categoria = await _categoriaService.GetCategoriaId(id);
+
+            if (categoria == null)
+                return NotFound("Categoria não existe");
+
+            return Ok(categoria);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CategoriaDTO categoria)
+        {
+            if (categoria == null)
+                return BadRequest("Invalid Data");
+
+            await _categoriaService.Add(categoria);
+
+            return new CreatedAtRouteResult("GetCategoria", new { id = categoria.Id }, categoria );
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(int id, [FromBody] CategoriaDTO categoria) 
+        {
+            if (id != categoria.Id)
+                return BadRequest();
+
+            if (categoria == null)
+                return BadRequest();
+
+          await _categoriaService.Update(categoria);
+
+            return Ok(categoria);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<CategoriaDTO>> Delete(int id)
+        {
+            var categoria = _categoriaService.GetCategoriaId(id);
+
+            if (categoria.Result == null)
+                return NotFound("Categoria não encontrada.");
+
+            await _categoriaService.Remove(id);
+            return Ok(categoria);
+
         }
     }
 }
---
+++ CleanArch.API/Controllers/TokenController.cs
@@ -0,0 +1,77 @@
+﻿using CleanArch.API.Models;
+using CleanArch.Domain.Account;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAutenthicate _authenticate;
+        private readonly IConfiguration _configuration;
+
+        public TokenController(IAutenthicate authenticate, IConfiguration configuration)
+        {
+            _authenticate = authenticate ?? throw new ArgumentNullException(nameof(authenticate));
+            _configuration = configuration;
+        }
+
+        [HttpPost("LoginUser")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel userInfo)
+        {
+            var result = await _authenticate.Authenticate(userInfo.Email, userInfo.Password);
+
+            if (result)
+                return GenerateToken(userInfo);
+               // return Ok($"User { userInfo.Email} login sucesses");
+            else
+            {
+                ModelState.AddModelError(string.Empty, "invalid login");
+                return BadRequest();
+            }
+        }
+
+        private UserToken GenerateToken(LoginModel userInfo)
+        {
+
+            var claims = new[]
+            {
+              new Claim("email", userInfo.Email),
+              new Claim("meuValor", "oque quiser"),
+              new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var privatekey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:SecretKey"]));
+
+            var crendetials = new SigningCredentials(privatekey, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddMinutes(10);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                 issuer: _configuration["JWT:Issuer"],
+                 audience: _configuration["JWt:Audience"],
+                 claims: claims,
+                 expires: expiration,
+                 signingCredentials:crendetials
+                );
+
+            return new UserToken()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
+
+        }
+    }
+}
